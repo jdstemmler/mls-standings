cf6b7db3be47cdb8cfcbbc757df0e6c45a50d4ad
Jayson Stemmler <jstemm@uw.edu>
2015-06-28 22:58:44 -0700

    modified the code structure of mlsStandings a little bit, and added a new figure to plotStandings which shows the breakout by conference

--- mlsStandings.py
+++ mlsStandings.py
@@ -8,57 +8,62 @@ import os
 panel = os.path.join('./data/mlsStandings.data')
 has_panel = os.path.isfile(panel)
 
-# defile the url for the supporters shield standings
-url = "http://www.mlssoccer.com/standings/supporters-shield"
-html = urlopen(url)
-listingObj = BeautifulSoup(html.read())
+def supporters_shield(panel, has_panel):
+	# defile the url for the supporters shield standings
+	url = "http://www.mlssoccer.com/standings/supporters-shield"
+	html = urlopen(url)
+	listingObj = BeautifulSoup(html.read())
 
-# get the last updated time and convert to python datetime
-update = (listingObj.findAll("div", {"class": "standings-last-update"})[0]
-			.contents[0]
-			.strip()
-			.split('Updated:')[-1]
-			.strip())
-last_update = datetime.datetime.strptime(update, '%m/%d/%y at %I:%M %p')
+	# get the last updated time and convert to python datetime
+	update = (listingObj.findAll("div", {"class": "standings-last-update"})[0]
+				.contents[0]
+				.strip()
+				.split('Updated:')[-1]
+				.strip())
+	last_update = datetime.datetime.strptime(update, '%m/%d/%y at %I:%M %p')
 
-# get the standings table and find all rows
-table = listingObj.findAll("table", {"class": "standings-table"})[0]
-rows = table.findAll("tr")
+	# get the standings table and find all rows
+	table = listingObj.findAll("table", {"class": "standings-table"})[0]
+	rows = table.findAll("tr")
 
-# pull out the headers
-headers = [r.contents[0].strip() for r in rows[0].findChildren("th")]
+	# pull out the headers
+	headers = [r.contents[0].strip() for r in rows[0].findChildren("th")]
 
-# initialize data structures
-data = None
-clubs = []
+	# initialize data structures
+	data = None
+	clubs = []
 
-# iterate through each row, add to data dictionary
-for row in rows[1:]:
-	if data is None:
-		data = {}
+	# iterate through each row, add to data dictionary
+	for row in rows[1:]:
+		if data is None:
+			data = {}
 
-	rank = (int(row.findChild("td", {"class": "first"})
-						.contents[0]
-						.strip()))
+		rank = (int(row.findChild("td", {"class": "first"})
+							.contents[0]
+							.strip()))
 
-	clubs.append(row.findChild("a").contents[0].strip())
-	values = [float(r.contents[0].strip()) for r in row.findChildren("td")[2:]]
+		clubs.append(row.findChild("a").contents[0].strip())
+		values = [float(r.contents[0].strip()) for r in row.findChildren("td")[2:]]
 
-	for k, v in zip(['rank']+headers[2:], [rank]+values):
-		if k not in data.keys():
-			data[k] = []
+		for k, v in zip(['rank']+headers[2:], [rank]+values):
+			if k not in data.keys():
+				data[k] = []
 
-		data[k].append(v)
+			data[k].append(v)
 
-# save the data to a DataFrame
-D = pd.DataFrame(data, index=clubs).sort()
+	# save the data to a DataFrame
+	D = pd.DataFrame(data, index=clubs).sort()
 
-# check for the Panel. Create if it doesn't exist.
-if not has_panel:
-	P = pd.Panel({last_update: D})
-elif has_panel:
-	P = pd.read_hdf(panel, 'data')
-	P[last_update] = D
+	# check for the Panel. Create if it doesn't exist.
+	if not has_panel:
+		P = pd.Panel({last_update: D})
+	elif has_panel:
+		P = pd.read_hdf(panel, 'shield')
+		P[last_update] = D
 
-# write the panel out to file.
-P.to_hdf(panel, 'data')
+	# write the panel out to file.
+	P.to_hdf(panel, 'shield')
+
+	return P
+
+P = supporters_shield(panel, has_panel)
--- plotStandings.py
+++ plotStandings.py
@@ -7,9 +7,13 @@ import seaborn as sns
 
 
 panel = os.path.join('./data/mlsStandings.data')
-P = pd.read_hdf(panel, 'data')
+P = pd.read_hdf(panel, 'shield')
 
 colors = os.path.join('./assets/teamColors.csv')
+conferences = os.path.join('./assets/conferences.csv')
+
+confcolors = {'Western': '#FA5858',
+              'Eastern': '#81BEF7'}
 
 def read_colors(colors):
     colortable = pd.read_csv(colors, header=0, index_col="Club")
@@ -17,6 +21,13 @@ def read_colors(colors):
     for club, r, g, b in colortable.itertuples():
         colordict[club] = (r/255., g/255., b/255.)
     return colordict
+
+def read_conference(conferences):
+    conftable = pd.read_csv(conferences, header=0, index_col="Club")
+    confdict = {}
+    for club, conference in conftable.itertuples():
+        confdict[club] = conference.strip()
+    return confdict
 
 fig = plt.figure()
 ax = fig.add_axes([.1, .1, .6, .8])
@@ -27,16 +38,55 @@ standings = standings.sort(dates[-1], ascending=False)
 clubs = standings.index.tolist()
 
 colordict = read_colors(colors)
+confdict = read_conference(conferences)
 
 for club in clubs:
-    ax.plot_date(dates, standings.ix[club].values, '.-',
+    ax.plot_date(dates, standings.ix[club].values, '-',
                  label=club, color=colordict[club])
     #standings.ix[club].plot('.-', ax=ax, label=club)
 
-ax.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0., fontsize=9)
+ax.legend(bbox_to_anchor=(1, .5), loc='center left',
+          borderaxespad=0., fontsize=8)
 ax.set_ylim(top = ax.get_ylim()[-1]+5)
 ax.set_ylabel('Points')
 
-fig.suptitle('Current MLS Standings\nAs of {}'.format(dates[-1].date()))
+fig.suptitle("Current MLS Supporter's Shield Standings\nAs of {} Eastern"
+             .format(dates[-1]))
 fig.autofmt_xdate()
-fig.savefig('./plots/standings.png', dpi=300)
+fig.savefig('./plots/standings_history.png', dpi=300)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+for i, club in enumerate(clubs):
+    if confdict[club] == "Western":
+        scale = -1
+        ha = 'right'
+    elif confdict[club] == "Eastern":
+        scale = 1
+        ha = 'left'
+
+    points = standings[dates[-1]].ix[club]
+
+    ax.barh(-1*i, scale*points, align='center',
+            color=confcolors[confdict[club]])
+    ax.text(scale*2, -1*i, club, ha=ha, va='center', fontsize=8)
+    ax.text(scale*(points+1), -1*i, '{:2.0f}'.format(points),
+            ha=ha, va='center', fontsize=9)
+
+ax.set_ylim(top=1)
+ax.set_xticklabels(['{:2.0f}'.format(xt) for xt in abs(ax.get_xticks())])
+ax.set_yticklabels([])
+ax.set_xlabel('Points')
+ax.grid('off', axis='y')
+ax.set_ylabel('Western Conference')
+ax2 = ax.twinx()
+ax2.set_ylabel('Eastern Conference')
+ax2.grid('off')
+ax2.set_yticks([])
+ax.set_title('MLS Standings by Conference\nAs of {} Eastern'.format(dates[-1]))
+
+fig.savefig('./plots/standings_current.png')
+
+plt.close('all')
