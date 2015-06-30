faaf66f6890bbae80e8ef847c885bdc8f23923ae
Jayson Stemmler <jdstemmler@gmail.com>
2015-06-29 18:55:04 -0700

    changed plot dpi

--- makePlots.py
+++ makePlots.py
@@ -10,7 +10,7 @@ conferences = os.path.join('./assets/conferences.csv')
 
 # Make the line plot with all the teams and points for each game
 fig, ax = mlsPlots.plot_points_by_game(T, colors)
-fig.savefig('./plots/standings_by_game.png', dpi=150)
+fig.savefig('./plots/standings_by_game.png', dpi=300)
 
 # make the bar chart of total current points
 points = T.sum().copy()
@@ -18,7 +18,7 @@ points.sort(ascending=False)
 
 fig, ax = mlsPlots.plot_split_standings(points, conferences)
 ax.set_title("MLS Club Standings by Conference\nTotal Current Points")
-fig.savefig('./plots/current_points_standings.png', dpi=150)
+fig.savefig('./plots/current_points_standings.png', dpi=300)
 
 level_game_points = T.dropna(how='any', axis=0)
 level_games = level_game_points.index[-1]
@@ -29,4 +29,4 @@ fig, ax = mlsPlots.plot_split_standings(level_game_points, conferences,
                                         secondary=points)
 ax.set_title("MLS Club Standings by Conference\nGame {:2.0f}"
              .format(level_games))
-fig.savefig('./plots/level_game_points_standings.png', dpi=150)
+fig.savefig('./plots/level_game_points_standings.png', dpi=300)
