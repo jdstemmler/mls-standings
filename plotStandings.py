2d54c22964915e31f43a8c8b932c7250cb3308b9
Jayson Stemmler <jdstemmler@gmail.com>
2015-06-29 18:52:26 -0700

    made updates to the plots

--- plotStandings.py
+++ plotStandings.py
@@ -40,16 +40,28 @@ ax = fig.add_axes([.1, .1, .6, .8])
 
 current_points = T.sum().copy()
 current_points.sort(ascending=False)
-clubs = current_points.index.tolist()
+
+level_points = T.dropna(how='any', axis=0)
+level_games = level_points.index[-1]
+level_points = level_points.sum()
+level_points.sort(ascending=False)
+
+points_per_game = P[-1, :, "PPG"].copy()
+points_per_game.sort(ascending=False)
+
+clubs = points_per_game.index.tolist()
 
 colordict = read_colors(colors)
 confdict = read_conference(conferences)
+
+def split_bars(metric=None):
+    pass
+
 
 for club in clubs:
     club_standing = T[club].cumsum()
     ax.plot(club_standing.index, club_standing.values, '.-',
                  label=club, color=colordict[club])
-    #standings.ix[club].plot('.-', ax=ax, label=club)
 
 ax.legend(bbox_to_anchor=(1, .5), loc='center left',
           borderaxespad=0., fontsize=8)
@@ -59,9 +71,8 @@ ax.set_ylabel('Points')
 
 ax.set_title("Current MLS Supporter's Shield Standings\nAs of {} Eastern"
              .format(P.items[-1]))
-#fig.autofmt_xdate()
 fig.savefig('./plots/standings_history.png', dpi=300)
-
+# --------------------------------------------------------------------
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -78,10 +89,17 @@ for i, club in enumerate(clubs):
         if ep is not None: ep += 1
 
     points = current_points.ix[club]
+    ppg = P[-1, club, "PPG"]
+    games_played = P[-1, club, "GP"]
+    game_differential = games_played - level_games
 
-    ax.barh(-1*i, scale*points, align='center',
+    ax.barh(-1*i, scale*ppg, align='center',
             color=confcolors[confdict[club]],
-            edgecolor='none')
+            edgecolor='none',
+            alpha=1)
+    #ax.barh(-1*i, scale*level_points[club], align='center',
+    #        color=confcolors[confdict[club]],
+    #        edgecolor='none')
 
     if wp == 6:
         ax.plot([0, ax.get_xlim()[0]], [-1*i-.5, -1*i-.5], 'k--', linewidth=.5)
@@ -90,21 +108,27 @@ for i, club in enumerate(clubs):
         ax.plot([0, ax.get_xlim()[1]], [-1*i-.5, -1*i-.5], 'k--', linewidth=.5)
         ep = None
 
-    ax.text(scale*2, -1*i, club, ha=ha, va='center', fontsize=8)
-    ax.text(scale*(points+1), -1*i, '{:2.0f}'.format(points),
-            ha=ha, va='center', fontsize=9)
+    ax.text(scale/10, -1*i, club, ha=ha, va='center', fontsize=8)
+    ax.text(scale*(ppg+.01), -1*i, '{:1.4}'.format(ppg),
+            ha=ha, va='center', fontsize=6)
+    #ax.text(scale*(points+1), -1*i,
+    #        '{:1.0f} [{:1.0f}/{:1.0f}] +{:1.0f}'.format(points,
+    #                                 level_points[club],
+    #                                 points-level_points[club],
+    #                                 game_differential),
+    #        ha=ha, va='center', fontsize=6)
 
 ax.set_ylim(top=1)
-ax.set_xticklabels(['{:2.0f}'.format(xt) for xt in abs(ax.get_xticks())])
+ax.set_xticklabels(['{:2.1f}'.format(xt) for xt in abs(ax.get_xticks())])
 ax.set_yticklabels([])
-ax.set_xlabel('Points')
+ax.set_xlabel('Points per Game')
 ax.grid('off', axis='y')
 ax.set_ylabel('Western Conference')
 ax2 = ax.twinx()
 ax2.set_ylabel('Eastern Conference')
 ax2.grid('off')
 ax2.set_yticks([])
-ax.set_title('MLS Standings by Conference\nAs of {} Eastern'.format(P.items[-1]))
+ax.set_title('MLS Standings by Conference\nBy Points per Game')
 
 fig.savefig('./plots/standings_current.png', dpi=300)
 
